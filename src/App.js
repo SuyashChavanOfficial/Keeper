be9d10ba8c19c8ad6cd850eff8df15892de0b6c4
Suyash Chavan <suyashchavanofficial@gmail.com>
2024-06-11 20:31:41 +0530

    Created Footer and Note Component

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Heading from './components/Heading'
+import Note from './components/Note';
+import Footer from './components/Footer';
+
+function App() {
+    return(
+        <>
+            <Heading />
+            <Note />
+            <Footer />
+        </>
+    );
+}
+
+export default App;
